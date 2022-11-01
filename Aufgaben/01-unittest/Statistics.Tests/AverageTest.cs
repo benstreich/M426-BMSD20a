097dc6ecf693abac2730e7683d2c0b39e47aaad7
Ben Streich <ben.streich@icloud.com>
2022-11-01 19:46:17 +0100

    Aufgabe 5: Arbeit vollbracht in Average.cs, AverageTest.cs. Arbeit habe ich zusammengemacht mi Lian Eifert

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 
+
+
 namespace Statistics.Tests
 {
     public class AverageTest
@@ -9,11 +11,60 @@ namespace Statistics.Tests
         [Fact]
         public void TestMeanOfTwoNumbers()
         {
-            List<int> numbers = new List<int> { 1 /*3*/, 3 /*2*/ };
-            double expected = 2; //2.5
+            List<int> numbers = new List<int> { 1/*3*/, 3 /*2*/};
+            double expected = 2/*2,5*/;
             Average average = new Average();
 
+
+
             double actual = average.Mean(numbers);
+
+
+
+            Assert.Equal(expected, actual);
+        }
+
+
+
+        [Fact]
+        public void TestMeanOfFiveNumbers()
+        {
+            List<int> numbers = new List<int> { 4, 4, 10, 10, 7 };
+            double expected = 7;
+            Average average = new Average();
+
+
+
+            double actual = average.Mean(numbers);
+
+
+
+            Assert.Equal(expected, actual);
+        }
+
+
+
+        [Fact]
+        public void TestMedianForOddNumberOfElements()
+        {
+            List<int> numbers = new List<int> {/*1,*/1, 2, 3, 4, 5, 6, 7/*,200*/}; //Es geht immer noch
+            double expected = 4;
+            Average average = new Average();
+            double actual = average.Median(numbers);
+
+
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestMedianForEvenNumberOfElements()
+        {
+            List<int> numbers = new List<int> { /*1*/400, 6, 5, 2, 8, 11, 10, 2,/*, 10000*/ }; //Es geht immer noch
+            double expected = 7;
+            Average average = new Average();
+            double actual = average.Median(numbers);
+
+
 
             Assert.Equal(expected, actual);
         }
--- Aufgaben/01-unittest/Statistics/Average.cs
+++ Aufgaben/01-unittest/Statistics/Average.cs
@@ -1,22 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
+
 namespace Statistics
 {
     public class Average
     {
         public double Mean(List<int> numbers)
         {
+            if (numbers.Count == 0)
+            {
+                throw new ArgumentException();
+            }
             int sum = 0;
             double mean = 0.0;
+
+
 
             foreach (int number in numbers)
             {
                 sum += number;
             }
 
+
+
             mean = (double)sum / numbers.Count;
             return mean;
         }
+
+
+
+        public double Median(List<int> numbers)
+        {
+
+
+
+            numbers.Sort();
+            
+            if (numbers.Count % 2 == 0)
+            {
+                return (double)(numbers[(int)numbers.Count / 2] + numbers[((int)numbers.Count / 2) - 1]) / 2;
+            }
+            
+
+            else
+            {
+                return numbers[(int)Math.Floor((double)numbers.Count / 2)];
+            }
+
+
+
+
+        }
+
+
+
+
     }
 }
